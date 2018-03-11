c1077f5e100b02b54fdc36ac08f92b53e98e593d
Dogeee <26361994+Dogeee@users.noreply.github.com>
2018-03-11 12:07:28 +0000

    Update ElderGuardian.php

--- src/revivalpmmp/pureentities/entity/monster/swimming/ElderGuardian.php
+++ src/revivalpmmp/pureentities/entity/monster/swimming/ElderGuardian.php
@@ -35,20 +35,18 @@ class ElderGuardian extends SwimmingMonster implements Monster{
 		parent::attackEntity($player);
 	}
 	
-	public function getDrops(){
-		$drops = [ItemItem::get(ItemItem::PRISMARINE_SHARD, 0, mt_rand(0, 2))];
-		
-		if($this->getLastDamageCause() === EntityDamageEvent::CAUSE_FIRE){
-			$drops[] = ItemItem::get(ItemItem::COOKED_FISH, 0, mt_rand(0, 1));
+        public function getDrops() : array{
+		$drops = [];
+		if($this->isLootDropAllowed()){
+			switch(mt_rand(0, 1)){
+				case 0:
+					$drops[] = Item::get(Item::PRISMARINE_SHARD, 0, 1);
+					break;
+				case 1:
+					$drops[] = Item::get(Item::PRISMARINE_CRYSTAL, 0, 1);
+					break;
+			}
 		}
-		else{
-			$drops[] = ItemItem::get(ItemItem::RAW_FISH, 0, mt_rand(0, 1));
-		}
-		
-		$drops[] = ItemItem::get(ItemItem::PRISMARINE_CRYSTAL, 0, mt_rand(0, 100) < 33?1:0);
-		
-		$drops[] = ItemItem::get(ItemItem::SPONGE, 1);
-		
 		return $drops;
 	}
 
