c9518e8a7c659b41e4504ad940021f4125081c05
Dogeee <26361994+Dogeee@users.noreply.github.com>
2018-03-11 11:58:04 +0000

    EnderGuardians now drop items.

--- src/revivalpmmp/pureentities/entity/monster/swimming/ElderGuardian.php
+++ src/revivalpmmp/pureentities/entity/monster/swimming/ElderGuardian.php
@@ -34,6 +34,23 @@ class ElderGuardian extends SwimmingMonster implements Monster{
 	public function attackEntity(Entity $player){
 		parent::attackEntity($player);
 	}
+	
+	public function getDrops(){
+		$drops = [ItemItem::get(ItemItem::PRISMARINE_SHARD, 0, mt_rand(0, 2))];
+		
+		if($this->getLastDamageCause() === EntityDamageEvent::CAUSE_FIRE){
+			$drops[] = ItemItem::get(ItemItem::COOKED_FISH, 0, mt_rand(0, 1));
+		}
+		else{
+			$drops[] = ItemItem::get(ItemItem::RAW_FISH, 0, mt_rand(0, 1));
+		}
+		
+		$drops[] = ItemItem::get(ItemItem::PRISMARINE_CRYSTAL, 0, mt_rand(0, 100) < 33?1:0);
+		
+		$drops[] = ItemItem::get(ItemItem::SPONGE, 1);
+		
+		return $drops;
+	}
 
 	public function getName() : string{
 		return "ElderGuardian";
